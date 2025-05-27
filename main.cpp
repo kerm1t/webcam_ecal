6a1c4ee174574553302b39ddb1e47c091f06f00a
kerm1t <kerm1t@anigators.com>
2025-05-27 15:58:39 +0200

    publish webcam as jpg

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ using namespace cv;
 // eCAL
 #include <ecal/ecal.h>
 #include <ecal/msg/protobuf/publisher.h>
-#include <ecal/msg/protobuf/subscriber.h>
 // proto i/o
 #include "webcam.pb.h"
+
+#include <vector> // For std::vector
 
 eCAL::protobuf::CPublisher<pb::webcam::webcam_raw> publisher_wcamr;
 int frameid;
@@ -19,17 +20,39 @@ void init_eCAL(int argc, char** argv) {
   // Initialize eCAL
   eCAL::Initialize(argc, argv, "Webcam->eCAL");
   // create publisher
-  publisher_wcamr = eCAL::protobuf::CPublisher<pb::webcam::webcam_raw>("WEBCAM_RAW"); // mono or stereo-left
+//  publisher_wcamr = eCAL::protobuf::CPublisher<pb::webcam::webcam_raw>("WEBCAM_RAW");
+  publisher_wcamr = eCAL::protobuf::CPublisher<pb::webcam::webcam_raw>("webcam_jpg");
   printf("Ecal publisher created\n");
   // set eCAL state to healthy (--> eCAL Monitor)
-  eCAL::Process::SetState(proc_sev_healthy, proc_sev_level1, "Webcam eCAL publisher initialized");
+  eCAL::Process::SetState(proc_sev_healthy, proc_sev_level1, "Webcam eCAL pub-node initialized");
 }
 
 void exit_eCAL() {
   eCAL::Finalize(); // finalize eCAL API
 }
 
-void publish_cam(const cv::Mat &frame) {
+void convertToJpeg(const cv::Mat& frame, std::vector<uchar>& jpegBuffer) {
+    // Encode the image to JPEG format
+    std::vector<int> compressionParams = {cv::IMWRITE_JPEG_QUALITY, 90}; // Set JPEG quality to 90
+    cv::imencode(".jpg", frame, jpegBuffer, compressionParams);
+}
+
+void publish_camjpg(const cv::Mat &frame) {
+    printf("frame %d --> rows=%d, cols=%d, chan's=%d\n",frameid, frame.rows, frame.cols, frame.channels());
+    pb::webcam::webcam_raw wcamj;
+
+    // Convert frame to JPEG
+    std::vector<uchar> jpegBuffer;
+    convertToJpeg(frame, jpegBuffer);
+    int size = jpegBuffer.size();
+ 
+    wcamj.set_payload(reinterpret_cast<char*>(jpegBuffer.data()), jpegBuffer.size());
+    wcamj.set_frame_id(++frameid);
+    wcamj.set_size(size); // w x h
+    publisher_wcamr.Send(wcamj);
+};
+
+void publish_camraw(const cv::Mat &frame) {
     printf("frame %d --> rows=%d, cols=%d, chan's=%d\n",frameid, frame.rows, frame.cols, frame.channels());
     int size = frame.rows*frame.cols;
     pb::webcam::webcam_raw wcamr;
@@ -38,6 +61,7 @@ void publish_cam(const cv::Mat &frame) {
     wcamr.set_size(size); // w x h
     publisher_wcamr.Send(wcamr);
 };
+
 #endif // eCAL_ON
 // <------- eCAL ---------->
 
@@ -58,6 +82,8 @@ int main(int argc, char** argv)
     VideoCapture cap(device); // open the camera <device>
     if(!cap.isOpened())  // check if we succeeded
         return -1;
+//    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1024);
+//    cap.set(cv::CAP_PROP_FRAME_HEIGHT,768);
 
     Mat edges;
     namedWindow("webcam->eCAL",1);
@@ -65,13 +91,10 @@ int main(int argc, char** argv)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
-//        cvtColor(frame, edges, COLOR_BGR2GRAY);
-//        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-//        Canny(edges, edges, 0, 30, 3);
-//        imshow("edges", edges);
-        imshow("webcam->eCAL", frame);
+///        imshow("webcam->eCAL", frame);
 
-        publish_cam(frame);
+//        publish_camraw(frame);
+        publish_camjpg(frame);
 
         if(waitKey(30) >= 0) break;
     }
